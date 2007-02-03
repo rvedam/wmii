86b7c968a1da41dcb6b0d799fe7936762610ce17
Denis Grelich <denisg@suckless.org>
2007-02-03 18:42:40 +0100

    no -O2 for debug anymore

---
+++ config.mk
@@ -0,0 +1,33 @@
+# wmii version
+VERSION = 3.5.1
+CONFVERSION = 3.5
+
+# Customize below to fit your system
+
+# paths
+PREFIX = /usr/local
+CONFPREFIX = ${PREFIX}/etc
+MANPREFIX = ${PREFIX}/share/man
+
+X11INC = /usr/X11R6/include
+X11LIB = /usr/X11R6/lib
+
+# includes and libs
+INCS = -I. -I${PREFIX}/include -I/usr/include -I${X11INC}
+LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 -L${PREFIX}/lib -lixp -lm
+
+# flags
+#CFLAGS = -Os ${INCS} -DVERSION=\"${VERSION}\"
+#LDFLAGS = ${LIBS}
+CFLAGS = -g -Wall ${INCS} -DVERSION=\"${VERSION}\"
+LDFLAGS = -g ${LIBS}
+
+# Solaris
+#CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
+#LDFLAGS = ${LIBS} -R${PREFIX}/lib
+#LDFLAGS += -lsocket -lnsl
+#CFLAGS += -xtarget=ultra
+
+# compiler and linker
+CC = cc
+LD = ${CC}
